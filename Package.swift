c43acceab9c8ffdbe534cc71667c3737ab1fdf00
elmetal <nm7.pupu@gmail.com>
2025-02-12 14:05:24 +0900

    Revert "use swift-docc-plugin"
    This reverts commit 3453582c1404c101f8b1852c460ddf7e5ea70461.

--- Package.swift
+++ Package.swift
@@ -11,9 +11,6 @@ let package = Package(
             name: "SwiftUIViewCodingGuidelines",
             targets: ["SwiftUIViewCodingGuidelines"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin.git", exact: "1.4.3")
-    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
