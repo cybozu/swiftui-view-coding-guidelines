99e1be42c6aecaa842cc4d02d83f403e41e90320
elmetal <nm7.pupu@gmail.com>
2025-02-12 14:11:57 +0900

    use archive-docc.sh

---
+++ archive-docc.sh
@@ -0,0 +1,14 @@
+#/usr/bin/env bash -euo pipefail
+
+DOCC_COMMAND="/usr/bin/xcrun docc"
+DOCC_ARCHIVE="Guidelines.doccarchive"
+
+$DOCC_COMMAND convert Guidelines.docc \
+  --fallback-display-name SwiftUIViewCodingGuidelines \
+  --fallback-bundle-identifier org.cybozu.SwiftUIViewCodingGuidelines \
+  --fallback-bundle-version 1 \
+  --output-dir $DOCC_ARCHIVE
+
+$DOCC_COMMAND process-archive transform-for-static-hosting \
+  --output-path ./docs \
+  $DOCC_ARCHIVE
